97200f5543b0fcdd60ef0c32ffb84719d9c3c125
Feynstein <yohan.belanger.42@gmail.com>
2020-05-09 21:52:51 -0400

    Further progress in trolley data container

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -18,10 +18,20 @@
 // Ctor needed to setup all the default variables
 Parameters::Parameters()
 {
+    // Need to check if anys can be instanciated this way without being cast in their final state ... so yeah make sure we dont get casting errors down the road
+    m_dataMap["PlaceCloseIndepThresh"] = (int)0;
+    m_dataMap["LoadSaveNetwork"] = (int)0;
+    m_dataMap["DoHeteroDensity"] = (int)0;
+    m_dataMap["DoPeriodicBoundaries"] = (int)0;
+    m_dataMap["DoSchoolFile"] = (int)0;
+    m_dataMap["DoAdunitDemog"] = (int)0;
+    m_dataMap["OutputDensFile"] = (int)0;
+    m_dataMap["OutputDensFile"] = (int)0;
+    m_dataMap["MaxNumThreads"] = (int)0; // Doesnt seem right ... 
+    m_dataMap["DoInterventionFile"] = (int)0;
+    m_dataMap["PreControlClusterIdCaseThreshold"] = (int)0;
+    m_dataMap["R0scale"] = (double) 1.0;
 
-    m_dataMap["PlaceCloseIndepThresh"] = 0;
-    m_dataMap["LoadSaveNetwork"] = 0;
-    m_dataMap["DoHeteroDensity"] = 0;
 
     P.PlaceCloseIndepThresh = P.LoadSaveNetwork = P.DoHeteroDensity = P.DoPeriodicBoundaries = P.DoSchoolFile = P.DoAdunitDemog = P.OutputDensFile = P.MaxNumThreads = P.DoInterventionFile = 0;
 		P.PreControlClusterIdCaseThreshold = 0;
